06a1268e70449e873e68fd6b427854a75b139c5e
coolalves <alvesdaniel@ua.pt>
2022-01-12 19:39:50 +0000

    recuperação de password

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Dashboard from "./Dashboard"
 import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
-
+import ForgotPassword from "./ForgotPassword";
 
 function App() {
   return( 
@@ -30,6 +30,7 @@ function App() {
           />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
     </AuthProvider>
     </Router>
