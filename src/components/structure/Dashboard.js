dd1dd8de17d468f812448d84462b95468df47be4
coolalves <alvesdaniel@ua.pt>
2022-02-22 01:16:51 +0000

    search component

--- src/components/structure/Dashboard.js
+++ src/components/structure/Dashboard.js
@@ -15,8 +15,7 @@ import { logout, colRefSongs, useAuth, upload } from "../../config/firebase";
 //import { onSnapshot, query, where, docs } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { users } from "../../config/firebase";
-import { addDoc } from "firebase/firestore";
-import {songs} from "../../config/firebase"
+import { songs } from "../../config/firebase";
 
 //recoil
 import { useRecoilValue } from "recoil";
@@ -28,11 +27,14 @@ import { getEmail } from "../../recoil/selectors/getEmail";
 import { getUsers } from "../../recoil/selectors/getAllUsers";
 import { getUid } from "../../recoil/selectors/getUid";
 
+//components
+import Search from "./Search"
+
 export default function Dashboard() {
   const [loading, setLoading] = useState(false);
 
-  //const uid = window.sessionStorage.getItem("id");
-  const loggedname = window.sessionStorage.getItem("username");
+  
+  
   const navigate = useNavigate();
 
   const [photoURL, setPhotoURL] = useState(
@@ -86,71 +88,6 @@ export default function Dashboard() {
     }
   }, [currentUser]);
 
-  const [txtSong, setTxtSong] = useState("");
-  const [selectedSong, setSelectedSong] = useState("");
-  const [selectedSongInfo, setSelectedSongInfo] = useState([]);
-  const [song, setSong] = useState("");
-
-  const getSong = (e) => {
-    console.log(e);
-    setSelectedSongInfo(e)
-    setSelectedSong(
-      <div className="boxsongs">
-        <div className="songs">
-          <img src={e.album.cover_small}></img>
-
-          <div className="songInfo">
-            <h2>{e.title}</h2>
-
-            <h4>{e.artist.name}</h4>
-          </div>
-        </div>
-      </div>
-    );
-
-    setSong("");
-  };
-
-  var songList;
-  useEffect(() => {
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${txtSong}`, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        "x-rapidapi-key": "4bb5270137msh7798e1355b243c4p19e658jsna5b2299ba91e",
-      },
-    })
-      .then((response) => {
-        response.json().then((info) => {
-          songList = info.data.map((elmt) => {
-            return (
-              <div className="boxsongs">
-                <div className="songs">
-                  <img src={elmt.album.cover_small}></img>
-                  <div className="songInfo">
-                    <h2>{elmt.title}</h2>
-                    <h4>{elmt.artist.name}</h4>
-                  </div>
-                </div>
-                <button
-                  onClick={() => {
-                    getSong(elmt);
-                  }}
-                >
-                  Adicionar
-                </button>
-              </div>
-            );
-          });
-          console.log(info.data[0]);
-          setSong(songList);
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [txtSong]);
-
   if (loading) {
     return (
       <>
@@ -158,16 +95,7 @@ export default function Dashboard() {
       </>
     );
   }
-  function insertSong(){
-    addDoc(colRefSongs, {
-        id: selectedSongInfo.id,
-        image: selectedSongInfo.album.cover,
-        name: selectedSongInfo.artist.name,
-        recomendedby: username,
-        title: selectedSongInfo.title
-      })
-      //console.log(selectedSongInfo.id)
-  }
+
   return (
     <>
       <Card>
@@ -176,35 +104,7 @@ export default function Dashboard() {
           <h2 className="text-center mb-4">Welcome, {username} !</h2>
           <p className="text-center mb-4">{email}</p>
 
-          <div className="songs">
-                  <img src={songs[0].image}></img>
-                  <div className="songInfo">
-                    <h2>{songs[0].name}</h2>
-                    <h4>{songs[0].title}</h4>
-                  </div>
-        </div>
-
-          <Form.Group>
-            <Form.Label>Search</Form.Label>
-            <Form.Control
-              placeholder="Song name"
-              onChange={(e) => {
-                setTxtSong(e.target.value);
-              }}
-            />
-            <div>
-              {txtSong}
-              {song}
-            </div>
-
-            <div>
-              {selectedSong}
-              <button onClick={()=>{insertSong()}}>
-                recomendar
-              </button>
-            </div>
-          </Form.Group>
-
+          <Search username={username}/>   
           <div className="text-center w-100 text-center mt-3">
             <img
               style={{ borderRadius: 200, width: 65 }}
--- src/components/structure/Search.js
+++ src/components/structure/Search.js
@@ -1,7 +1,141 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { addDoc } from "firebase/firestore";
+import { colRefSongs } from "../../config/firebase";
 
-export default function Search() {
+import { getUser } from "../../recoil/selectors/getUsername";
+import { useRecoilValue } from "recoil";
+
+import "../../styles/app.css";
+
+const Search = (props) => {
+
+  //current date
+  let date = new Date()
+  let day = date.getDate()
+  let month = date.getMonth()
+  let year = date.getFullYear()
+  let hour = date.getHours()
+  let minutes = date.getMinutes()
+  let currentDate = day + '/' + (month + 1) + '/' + year + ' - ' + hour + 'h' +  minutes + 'm'
+
+  console.log(currentDate)
+
+
+  const [txtSong, setTxtSong] = useState("");
+  const [selectedSong, setSelectedSong] = useState("");
+  const [selectedSongInfo, setSelectedSongInfo] = useState([]);
+  const [song, setSong] = useState("");
+  //const username = useRecoilValue(getUser);
+  
+  const txtSongHandler = (e) => {
+    setTxtSong(e.target.value);
+
+    if (e.target.value === "") {
+      setSong("");
+    }
+  };
+
+  const getSong = (e) => {
+    console.log(e);
+    setSelectedSongInfo(e);
+    setSelectedSong(
+      <div className="boxsongs">
+        <div className="songs">
+          <img src={e.album.cover_small}></img>
+
+          <div className="songInfo">
+            <h2>{e.title}</h2>
+
+            <h4>{e.artist.name}</h4>
+          </div>
+        </div>
+      </div>
+    );
+
+    setSong("");
+  };
+
+  var songList;
+  useEffect(() => {
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${txtSong}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "4bb5270137msh7798e1355b243c4p19e658jsna5b2299ba91e",
+      },
+    })
+      .then((response) => {
+        response.json().then((info) => {
+          songList = info.data.map((elmt) => {
+            return (
+              <div className="boxsongs">
+                <div className="songs">
+                  <img src={elmt.album.cover_small}></img>
+                  <div className="songInfo">
+                    <h2>{elmt.title}</h2>
+                    <h4>{elmt.artist.name}</h4>
+                  </div>
+                </div>
+                <button
+                  onClick={() => {
+                    getSong(elmt);
+                  }}
+                >
+                  Adicionar
+                </button>
+              </div>
+            );
+          });
+          console.log(info.data[0]);
+          setSong(songList);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [txtSong]);
+
+  function insertSong() {
+    addDoc(colRefSongs, {
+      id: selectedSongInfo.id,
+      image: selectedSongInfo.album.cover,
+      name: selectedSongInfo.artist.name,
+      recomendedby: props.username,
+      title: selectedSongInfo.title,
+      date: currentDate
+    });
+    //console.log(selectedSongInfo.id)
+  }
+
   return (
-    <div>Search</div>
-  )
-}
+    <>
+      <label>Search</label>
+
+      <input
+        onChange={(e) => {
+          txtSongHandler(e);
+        }}
+        placeholder="Search"
+      ></input>
+
+      <div>
+        {txtSong}
+        {song}
+      </div>
+
+      <div>
+        {selectedSong}
+        <button
+          onClick={() => {
+            insertSong();
+          }}
+        >
+          recomendar
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Search;
